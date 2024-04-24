e836e2db35f7202da55a1bcca70fbc7b825d277c
bigjellyfish <bigjellyfish1127@gmail.com>
2024-04-24 23:12:54 +0800

    版面更大更新
    添加了用Jquery做的slider
    將Jquery和javascript做成同一個外部引用

--- js/index.js
+++ js/index.js
@@ -1,9 +1,42 @@
+//Javascript
 function donate(){
     var txt;
-    if(confirm("你真的要斗內喔?")){
-        txt = "但我沒地方給你斗內";
+    if(confirm("你真的要抖內喔?")){
+        txt = "但我沒地方給你抖內";
     } else{
         txt = "你好壞喔";
     }
     alert(txt);
 }
+function about(){
+    alert("這就是個關於我的網站,這按鈕放好玩的");
+}
+// JQuery
+
+$(document).ready(function(){
+    // RWD 漢堡選單
+    $('.menu-toggle').click(function(){
+        $('.nav').toggleClass('active');
+        $(this).toggleClass('active');
+        $('.nav .nav-link').removeClass('btn');
+    });
+    $('.nav-link').click(function(){
+        $('.nav').removeClass('active');
+        $('.menu-toggle').removeClass('active');
+        $('.nav .nav-link').addClass('btn');
+    });
+    // slider 滾動相簿
+    $(".owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            600:{
+                items:1
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
